0d2f595c02f98a0e09faf2ab401e2ad7843a1335
Danya D. Smith <danyadsmith@gmail.com>
2017-05-19 11:23:03 -0400

    Update Server to Utilize Custom Middleware
    * Add Custom Middleware for HTTP Headers

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const config = require('./config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-//const middleware = require('./middleware');
+const middleware = require('./middleware');
 
 const app = express();
 
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 // app.use(middleware.auth);
 
 app.use(morgan('dev'));
-// app.use(middleware.headers);
+app.use(middleware.headers);
 
 //app.use('/api/route', thisRouter);
 
