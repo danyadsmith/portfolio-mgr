3f4f13fc9f7ca0779428c49f5d5498e8bb5f3fa7
Danya D. Smith <danyadsmith@gmail.com>
2017-05-23 16:22:03 -0400

    Update Server Configuration
    * Require Authentication Router
    * Conditionally enable morgan logging if environment is not
      testing
    * Configure Express to use the Authentication Router

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const chalk = require('chalk');
 const config = require('./config');
 const express = require('express');
 const bodyParser = require('body-parser');
+const authRouter = require('./auth/authRoutes.js');
 const morgan = require('morgan');
 const middleware = require('./middleware');
 
@@ -12,10 +13,13 @@ app.use(bodyParser.json());
 
 // app.use(middleware.auth);
 
-app.use(morgan('dev'));
+if (config.env !== 'testing') {
+  app.use(morgan('dev'));
+}
+
 app.use(middleware.headers);
 
-//app.use('/api/route', thisRouter);
+app.use('/', authRouter);
 
 app.use(function (err, req, res, next) {
   console.error(chalk.red.bold('ERROR: ', err));
