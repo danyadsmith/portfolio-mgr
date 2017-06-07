fc6114b166297fff81cfb6374a48c41200e0b2fd
Danya D. Smith <danyadsmith@gmail.com>
2017-06-07 15:01:30 -0400

    Configure AngularJS Application
    Configure the core AngularJS application. Setup initial routes
    and views using ui-router. Setup initial controllers for Home and
    About pages.

---
+++ client/_/js/app.js
@@ -0,0 +1,24 @@
+/* globals angular */
+
+angular.module('portfolio', [
+  'portfolio.home',
+  'ui.router'
+])
+.config(function ($stateProvider, $locationProvider) {
+  $locationProvider
+    .html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+  $stateProvider
+    .state('home', {
+      name: 'home',
+      url: '/',
+      views: {
+        'headerView': {
+          controller: 'HomeController',
+          templateUrl: '../partials/home/header.html'
+        }
+      }
+    });
+});
