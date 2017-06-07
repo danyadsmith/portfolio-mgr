c3656e62ddd21256b5aa39d4f0b5d4554116b962
Danya D. Smith <danyadsmith@gmail.com>
2017-06-07 14:46:36 -0400

    Resolve Minor Linting Error
    Change quotes on line 8 to single quotes.

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var config = require('./server/config');
 app.listen(config.port, function () {
   console.log(chalk.blue('PORTFOLIO MANAGER..............................'));
   console.log('Server API is listening on port ' + config.port);
-  console.log('Client site accessible at ' + chalk.white(config.url + ":" + config.port));
+  console.log('Client site accessible at ' + chalk.white(config.url + ':' + config.port));
 });
