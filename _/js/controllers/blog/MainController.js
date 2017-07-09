89fd4fcf5c00581741680b871dff8eac74569020
Danya D. Smith <danyadsmith@gmail.com>
2017-07-09 18:32:10 -0400

    Experimental Implementation of Paging in Blog Controller
    Updates the Blog partial to include a footer section with an
    unordered list that contains a number for each page in the
    blogs collection.
    
    Updates the BlogController to include additional $scope
    variables that detect information about the blog collection
    including limit and offset.

--- _/js/controllers/blog/MainController.js
+++ _/js/controllers/blog/MainController.js
@@ -6,5 +6,10 @@ angular.module('blog.main', [])
     $scope.data = {};
     Blog.getBlogPosts().then(function (posts) {
       $scope.data.posts = posts.rows;
+      $scope.data.count = posts.count;
+      $scope.data.limit = posts.limit;
+      $scope.data.page = posts.page;
+      $scope.data.offset = posts.offset;
+      $scope.data.pages = [...Array(posts.count / posts.limit).keys()];
     });
   });
