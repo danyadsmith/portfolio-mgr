d118e95f21ccbadecbccc7a3629501f06a603a21
Danya D. Smith <danyadsmith@gmail.com>
2017-07-23 16:40:26 -0400

    Populate Global Footer
    Closes #11
    
    Adds three sections to the global footer:
    
    * A biography section that displays the user’s gravatar profile image
    * An icon-based employment/job-seeking status panel
    * The two most recent projects published to the portfolio

--- _/js/services/profile.js
+++ _/js/services/profile.js
@@ -16,7 +16,20 @@ angular.module('profile.service', [])
       });
     };
 
+    var getUserData = function (userID) {
+      userID = userID || 1;
+      return $http({
+        method: 'GET',
+        url: '/api/users/' + userID
+      }).then(function (response) {
+        data = response.data;
+        //console.log(JSON.stringify(data));
+        return data;
+      });
+    };
+
     return {
-      getSocialProfiles: getSocialProfiles
+      getSocialProfiles: getSocialProfiles,
+      getUserData: getUserData
     };
   });
--- client/assets/js/controllers/global/FooterController.js
+++ client/assets/js/controllers/global/FooterController.js
@@ -1,10 +1,16 @@
 /* globals angular */
 
 angular.module('global.footer', [])
-  .controller('FooterController', function ($scope, Profile) {
+  .controller('FooterController', function ($scope, Profile, Portfolio) {
     $scope.name = 'Footer';
     $scope.data = {};
+    Portfolio.getPortfolioItems().then(function (portfolios) {
+      $scope.data.portfolios = portfolios;
+    });
     Profile.getSocialProfiles().then(function (socialProfiles) {
       $scope.data.profiles = socialProfiles;
+    });
+    Profile.getUserData().then(function (userData) {
+      $scope.data.user = userData;
     });
   });
