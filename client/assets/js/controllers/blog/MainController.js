456554b7dc81116d597e5b7b6dfa451d3935fd8e
Danya D. Smith <danyadsmith@gmail.com>
2017-08-09 12:36:36 -0400

    Replace AngularJS Filter with QueryString Params
    Remove the custom $filter that loads all blog posts then filters by
    category name. Replace pagination and category filter with a function
    that utilizes new server api routes that include limit and overflow
    properties.
    
    Remove the blog/categories/ index.html page.

--- client/assets/js/controllers/blog/MainController.js
+++ client/assets/js/controllers/blog/MainController.js
@@ -1,62 +1,69 @@
 /* globals angular */
 
 angular.module('blog.main', [])
-  .controller('BlogController', ['$rootScope', '$scope', '$filter', '$location', 'Blog', function ($rootScope, $scope, $filter, $location, Blog) {
+  .controller('BlogController', ['$rootScope', '$scope', '$window', '$location', 'Blog', function ($rootScope, $scope, $window, $location, Blog) {
     var postCount = 0;
     $scope.name = 'Blog';
     $rootScope.data = {};
-    $rootScope.filterOnName = '';
+    $rootScope.category = $location.search().category || 0;
+    $rootScope.limit = $location.search().limit || 0;
+    $rootScope.offset = $location.search().offset || 0;
+    // $rootScope.limit = $stateParams.limit || 0;
+    // $rootScope.offset = $stateParams.offset || 0;
+    //$rootScope.filterOnName = '';
     $rootScope.currentPage = 1;
     $rootScope.pageSize = 3;
-    $rootScope.currentCategory = '';
-
-    Blog.getBlogPosts().then(function (posts) {
-      postCount = posts.count;
-      console.log(postCount);
-    }).then(function (postCount) {
-      Blog.getLatestBlogPosts(postCount).then(function (posts) {
-        $rootScope.data.posts = posts.rows;
-        $rootScope.data.count = posts.count;
-        $rootScope.data.limit = posts.limit;
-        $rootScope.data.offset = posts.offset;
-        $rootScope.data.pageCount = posts.pageCount;
-        $rootScope.data.pages = posts.pages;
-        $rootScope.data.page = posts.page;
-        //console.log(posts);
-      });
-    });
-
+    $rootScope.currentCategory = null;
 
     Blog.getBlogCategories().then(function (categories) {
       $rootScope.data.categories = categories;
+    }).then(function () {
+      if ($rootScope.category > 0) {
+        Blog.getCategoryById($rootScope.category).then(function (category) {
+          $rootScope.data.category = category;
+          $rootScope.currentCategory = category.name;
+        }).then(function () {
+          console.log('Category Returned from Root Scope:', $rootScope.category);
+          Blog.getBlogPostsByCategory($rootScope.category, $rootScope.limit, $rootScope.offset).then(function (posts) {
+            $rootScope.data.posts = posts.rows;
+            $rootScope.data.count = posts.count;
+            $rootScope.data.limit = posts.limit;
+            $rootScope.data.offset = posts.offset;
+            $rootScope.data.pageCount = posts.pageCount;
+            $rootScope.data.pages = posts.pages;
+            $rootScope.data.page = posts.page;
+          });
+        });
+      } else {
+        console.log('No Category Returned from Root Scope');
+        Blog.getBlogPosts().then(function (posts) {
+          postCount = posts.count;
+          console.log(postCount);
+        }).then(function (postCount) {
+          Blog.getLatestBlogPosts(postCount).then(function (posts) {
+            $rootScope.data.posts = posts.rows;
+            $rootScope.data.count = posts.count;
+            $rootScope.data.limit = posts.limit;
+            $rootScope.data.offset = posts.offset;
+            $rootScope.data.pageCount = posts.pageCount;
+            $rootScope.data.pages = posts.pages;
+            $rootScope.data.page = posts.page;
+            //console.log(posts);
+          });
+        });
+      }
     });
 
     $scope.filterByCategory = function (category) {
-      console.log('Clicked!', category);
-      Blog.getBlogPostsByCategory(category.id).then(function (posts) {
-        $rootScope.data.posts = posts.rows;
-        $rootScope.data.count = posts.count;
-        $rootScope.data.pages = posts.pages;
-      });
-      $scope.filterOnName = category.name;
-      console.log($scope.filterOnName);
-      $rootScope.filterOnName = category.name;
-      console.log($location);
-      $location.url($location.path());
-      return $filter('filter')($rootScope.data.posts, $rootScope.filterOnName);
+      var categoryUrl = 'http://' + $window.location.host + '/blog/?category=' + category.id + '&limit=3&offset=0';
+      $window.location.href = categoryUrl;
     };
 
     $scope.pageChangeHandler = function (switchToPage) {
-      var currentPage = $rootScope.data.page;
-      var pageDiff = (switchToPage > currentPage) ? switchToPage - currentPage : currentPage - switchToPage;
-      var newOffset =  $rootScope.data.offset + ($rootScope.data.limit * pageDiff);
-      console.log('Current page: ', currentPage);
-      console.log('Current offset: ', $rootScope.data.offset);
-      console.log('Blog page changed to ' + switchToPage);
-      console.log('New offset:', newOffset);
-      $rootScope.data.offset = newOffset;
-      $rootScope.data.page = switchToPage;
-      $location.url($location.path());
-      return $filter('limitTo')($rootScope.data.posts, $rootScope.data.limit, newOffset);
+      var offset = (switchToPage === 1) ? 0 : (switchToPage * $rootScope.data.limit) - $rootScope.data.limit;
+      console.log('Changing to Page:', switchToPage);
+      console.log('Changing Page via Offset:', offset);
+      var categoryUrl = 'http://' + $window.location.host + '/blog/?category=' + $rootScope.category + '&limit=3&offset=' + offset;
+      $window.location.href = categoryUrl;
     };
   }]);
